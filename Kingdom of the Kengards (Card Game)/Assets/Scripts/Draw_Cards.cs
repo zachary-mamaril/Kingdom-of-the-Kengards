e2bb3cb8666b61668e9e1468c5eba2f215f8370d
Zachary Mamaril <zachary-mamaril@users.noreply.github.com>
2020-03-03 20:23:06 -0800

    Update 0.0.01A

---
+++ Kingdom of the Kengards (Card Game)/Assets/Scripts/Draw_Cards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_Cards : MonoBehaviour
+{
+    //Create public objects to use in the Inspector
+    public GameObject card1;
+    public GameObject card2;
+    public GameObject playerArea;
+    public GameObject enemyArea;
+
+    void Start()
+    {
+        
+    }
+
+}
